fbe2e0b06e2a0b156fab91a72840412e19dce428
EnriqueM <enrique.marr@gmail.com>
2014-12-03 17:38:54 -0600

    cleaned up the code for stage 1

--- stage1.js
+++ stage1.js
@@ -1,85 +1,55 @@
 var http = require('http');
-
-var stringToken = {
-  token: 'S5sRvIrSAH'
-};
-
-var tokenString = JSON.stringify(stringToken);
-
-var headers = {
-  'Content-Type': 'application/json',
-  'Content-Length': tokenString.length
-};
-
-var options = {
-  host: 'challenge.code2040.org',
-  path: '/api/getstring',
-  method: 'POST',
-  headers: headers
-};
-
-var reversedString = '', stringToReverse = '';
-
-// Request string to reverse, and reverse it when request is done
-var req = http.request(options, function(res) {
-  res.setEncoding('utf-8');
-
-  var responseString = '';
-
-  res.on('data', function(data) {
-    responseString += data;
-  });
-
-  res.on('end', function() {
-    var resultObject = JSON.parse(responseString);
-    stringToReverse = resultObject.result;
-    reversedString = stringToReverse.split('').reverse().join('');
-
-    // After doing the request and reversing the string,
-    // now it is time to send it back
-
-    var  reversedStringAndToken= {
-      token: 'S5sRvIrSAH',
-      string: reversedString
-    };
-
-    var stringToken = JSON.stringify(reversedStringAndToken);
-
-    var headers2 = {
+ 
+var token = 'S5sRvIrSAH'
+ 
+function reverse(str) {
+  return str.split('').reverse().join('');
+}
+ 
+function createOptions(method, content) {
+  return {
+    host: 'challenge.code2040.org',
+    path: '/api/' + method,
+    method: 'POST',
+    headers: {
       'Content-Type': 'application/json',
-      'Content-Length': stringToken.length
-    };
-
-    var options2 = {
-      host: 'challenge.code2040.org',
-      path: '/api/validatestring',
-      method: 'POST',
-      headers: headers2
-    };
-
-    req2 = http.request(options2, function(res2) {
-      res2.setEncoding('utf-8');
-
-      var responseString = '';
-
-      res2.on('data', function(data) {
-        responseString += data;
-      });
-
-      res2.on('end', function() {
-        var resultObject = JSON.parse(responseString);
-        console.log(resultObject);
-      });
+      'Content-Length': content.length
+    }
+  }
+}
+ 
+function sendPOST(data, method, callback) {
+  var content = JSON.stringify(data);
+  var options = createOptions(method, content);
+ 
+  var req = http.request(options, function(res) {
+    res.setEncoding('utf-8');
+   
+    var responseString = '';
+    res.on('data', function(data) {
+      responseString += data;
     });
-
-    req2.on('error', function(e) {
-      // TODO: handle error.
+   
+    res.on('end', function() {
+      var resultObject = JSON.parse(responseString);
+      callback(resultObject.result);
     });
-
-    req2.write(stringToken);
-    req2.end();
+  });
+ 
+  req.on('error', function(e) {
+    console.log(e);
+  });
+ 
+  req.write(content);
+  req.end();
+}
+ 
+// send post request to get the string, after that is finished, reverse the string
+// and send another post request to validate the string,
+// after that is finished, log the result
+sendPOST({ token: token }, 'getstring', function(result) {
+  var reversedString = reverse(result);
+  sendPOST({ token: token, string: reversedString }, 'validatestring', function(result) {
+    console.log(result);
   });
 });
-
-req.write(tokenString);
-req.end();
